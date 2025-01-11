7b9841c5f5ae0bb8d9e363f67b2d40ce8361440b
TheCraftlyShopOfficial <contact.craftly@gmail.com>
2025-01-12 03:15:20 +0530

    Update route.ts

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 import crypto from "crypto";
 import { NextResponse } from "next/server";
+import { PG } from "@/lib/pg";
 
-// Constants
-const salt_key: string = "96434309-7796-489d-8924-ab56988a6076";
-const merchant_id: string = "PGTESTPAYUAT86";
+
 
 export async function POST(req: Request): Promise<Response> {
   try {
@@ -14,24 +13,14 @@ export async function POST(req: Request): Promise<Response> {
     const merchantTransactionId: string = reqData.transactionId;
 
     // Prepare the payload
-    const data: {
-      merchantId: string;
-      merchantTransactionId: string;
-      name: string;
-      amount: number;
-      redirectUrl: string;
-      redirectMode: string;
-      callbackUrl: string;
-      mobileNumber: string;
-      paymentInstrument: { type: string };
-    } = {
-      merchantId: merchant_id,
+    const data = {
+      merchantId: PG.PG_MERCHENT_ID,
       merchantTransactionId: merchantTransactionId,
       name: reqData.name,
       amount: reqData.amount * 100, // Convert to paise (smallest currency unit)
-      redirectUrl: `http://localhost:3000/api/status?id=${merchantTransactionId}`,
-      redirectMode: "POST",
-      callbackUrl: `http://localhost:3000/api/status?id=${merchantTransactionId}`,
+      redirectUrl: PG.PG_REDIRECT_URL,
+      redirectMode: PG.PG_CALL_MODE,
+      callbackUrl: PG.PG_CALLBACK_URL,
       mobileNumber: reqData.mobile,
       paymentInstrument: {
         type: "PAY_PAGE",
@@ -44,18 +33,18 @@ export async function POST(req: Request): Promise<Response> {
 
     // Generate checksum
     const keyIndex:number = 1;
-    const string:string = payloadMain + "/pg/v1/pay" + salt_key;
+    const string:string = payloadMain + "/pg/v1/pay" + PG.PG_SALT_KEY;
     const sha256:string = crypto.createHash("sha256").update(string).digest("hex");
     const checksum:string = `${sha256}###${keyIndex}`;
 
     // Define PhonePe API URL
-    const prod_URL:string =
-      "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
+    
+      ;
 
     // API call options
     const options:any = {
       method: "POST",
-      url: prod_URL,
+      url: PG.PG_API_URL,
       headers: {
         accept: "application/json",
         "Content-Type": "application/json",
