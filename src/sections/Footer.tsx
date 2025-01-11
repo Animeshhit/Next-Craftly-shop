1634d264ad0ce9c23205d0464cf23033f5c9d449
TheCraftlyShopOfficial <contact.craftly@gmail.com>
2025-01-11 06:25:38 +0530

    Update Footer.tsx

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -35,8 +35,8 @@ const footerLinks = [
       { text: "Address :- Akulnagar", src: "/" },
       { text: "Murshidabad, West Bengal", src: "/" },
       { text: "India , 742302", src: "/" },
-      { text: "+91 85038 85083", src: "/", class: "underline" },
-      { text: "+91 9609096095", src: "/", class: "underline" },
+      { text: "+91 85038 85083 (24 x 7 Whatsapp Chatbot )", src: "https://wa.me/918503885083"} ,
+      { text: "+91 96090 96095", src: "https://919609096095" },
       {
         text: "thecraftlyshop@gmail.com",
         src: "mailto:thecraftlyshop@gmail.com",
