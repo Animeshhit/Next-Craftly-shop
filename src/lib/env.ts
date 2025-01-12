cbf10faeb135003644be6e3d255b12a7042ad95d
TheCraftlyShopOfficial <contact.craftly@gmail.com>
2025-01-13 01:55:45 +0530

    Update env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,10 +1,10 @@
-export const PG_MERCHENT_ID = process.env.PG_MERCHANT_ID;
-export const PG_SALT_KEY =    process.env.PHONEPE_SALT_KEY;
+export const PG_MERCHENT_ID = process.env.NEXT_PUBLIC_PG_MERCHANT_ID;
+export const PG_SALT_KEY =    process.env.NEXT_PUBLIC_PHONEPE_SALT_KEY;
 export const PG_SALT_INDEX =  process.env.PHONEPE_SALT_INDEX;
-export const PG_CALLBACK_URL = process.env.PHONEPE_CALLBACK_URL;
-export const PG_CALL_MODE  = process.env.PHONEPE_CALL_MODE ;
-export const PG_API_URL = process.env.PHONEPE_API_URL;
-export const PG_REDIRECT_URL = process.env.PHONEPE_REDIRECT_URL;
+export const PG_CALLBACK_URL = process.env.NEXT_PUBLIC_PHONEPE_CALLBACK_URL;
+export const PG_CALL_MODE  = process.env.NEXT_PUBLIC_PHONEPE_CALL_MODE ;
+export const PG_API_URL = process.env.NEXT_PUBLIC_PHONEPE_API_URL;
+export const PG_REDIRECT_URL = process.env.NEXT_PUBLIC_PHONEPE_REDIRECT_URL;
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-11-15";
 
 export const dataset = assertValue(
