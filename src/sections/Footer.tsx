6c62fed17893cc7eb67d1c68ce5c5804185b5b3c
TheCraftlyShopOfficial <contact.craftly@gmail.com>
2025-06-01 04:32:13 +0530

    Update Footer.tsx

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -87,7 +87,7 @@ const Footer = () => {
         ©2025 All rights reserved by <b>The Craftly Shop</b>
       </h3>
         <h3 className="text-center mt-4 font-inter text-sm py-1 tracking-tight text-black/85">
-        A brand of JS Group
+        A brand of JS Group by Snehasish Haldar
       </h3>
     </footer>
   );
